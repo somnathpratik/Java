228fb29fb328a6fd764c16f65ccde1707ca5ec22
Somnath Pratik <71363518+somnathpratik@users.noreply.github.com>
2022-05-02 17:59:14 +0530

    Add files via upload

---
+++ practicals/CharDataType.java
@@ -0,0 +1,11 @@
+package hello_world;
+
+public class CharDataType {
+public static void main(String args[]) {
+	char marks,grade;
+	marks='8';
+	grade='B';
+	System.out.println("Marks: "+marks);
+	System.out.println("Grade: "+grade);
+}
+}
