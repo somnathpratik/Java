76a4a1c0534f7c9a097b2d09a8709248d9caaf5b
Somnath Pratik <71363518+somnathpratik@users.noreply.github.com>
2022-05-05 18:03:16 +0530

    Add files via upload

---
+++ assignment/Practice.java
@@ -0,0 +1,23 @@
+package hello_world;
+
+public class Practice {
+public static void main (String args[]) {
+	char a = 'A';
+	byte b = 123;
+	short c = 2000;
+	int d = -5000;
+	long e = 200000L;
+	double f = 1234.1234d;
+	float g = 2000.44f;
+	boolean h = false;
+	System.out.println("My grade in maths is: "+a);
+	System.out.println("My bill of shampoo is: "+b);
+	System.out.println("He was born in the year: "+c);
+	System.out.println("The system is showing an error and printing a value of: "+d);
+	System.out.println("Rohan owes him an amout of: "+e);
+	System.out.println("The average amount of water supplied is: "+f);
+	System.out.println("The value of the 20 pounds is: "+g);
+	System.out.println("The boolean value is: "+h);
+	
+}
+}
