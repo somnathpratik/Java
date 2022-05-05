a60a7b1cdcfee17b15a6d3a00f271363ce224c4a
Somnath Pratik <71363518+somnathpratik@users.noreply.github.com>
2022-05-05 15:08:40 +0530

    Add files via upload

---
+++ assignment/Assignment1.java
@@ -0,0 +1,19 @@
+package hello_world;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+	
+		int n1,n2,add,sub,mul,div;
+		n1 = 250;
+		n2 = 5;
+		add = n1 + n2;
+		sub = n1 - n2;
+		mul = n1 * n2;
+		div = n1 / n2;
+		System.out.println("Num 1 : "+n1 + "\nNum2 : " + n2);
+		System.out.println("Addition : "+add);
+		System.out.println("Subtraction : "+sub);
+		System.out.println("Multiplication : "  +mul);
+		System.out.println("Division : "+div);
+	}
+}
