c6e0462f50633795088eb9571413dbf85b06b951
Somnath Pratik <71363518+somnathpratik@users.noreply.github.com>
2022-05-05 17:24:03 +0530

    Add files via upload

---
+++ practicals/BooleanDataType.java
@@ -0,0 +1,17 @@
+package hello_world;
+
+public class BooleanDataType
+
+{
+
+public static void main(String args[])
+
+{
+
+boolean myBool = true;
+
+if(myBool == true)
+
+System.out.println("I am using a Boolean data type:"+myBool);}
+
+}
---
+++ practicals/FloatDataType.java
@@ -0,0 +1,27 @@
+package hello_world;
+
+public class FloatDataType
+
+{
+
+public static void main(String args[])
+
+{
+
+float myFloat1,myFloat2,result;
+
+myFloat1=1000.666f;
+
+myFloat2=110.77f;
+
+result=myFloat1-myFloat2;
+
+System.out.println("Number1: "+myFloat1);
+
+System.out.println("Number2: "+myFloat2);
+
+System.out.println("Number1-Number2: "+result);
+
+}
+
+}
