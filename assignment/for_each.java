b8b3c9a3cdb209d2fef13a052ec5a407d76db8a9
Somnath Pratik <71363518+somnathpratik@users.noreply.github.com>
2022-05-13 17:24:54 +0530

    Add files via upload

---
+++ assignment/for_each.java
@@ -0,0 +1,11 @@
+class for_each{
+
+public static void main(String args[]){
+int a[] = {1,33,15,42,19,86,32,44,11,93,61,2,24,29};
+int sum = 0;
+for(int x:a){
+ sum+=x;
+}
+System.out.println("Sum of the array :"+sum);
+}
+} 
